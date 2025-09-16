f58dd11a95690e7fb43d28e3edd721ad1a1193b3
K3nski322 <kepenq@gmail.com>
2025-09-16 10:49:13 +0800

    Initial commit - Taskbuddy project

--- app/goals/index.jsx
+++ app/goals/index.jsx
@@ -1,26 +1,31 @@
-import { View, Text, StyleSheet } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { SafeAreaView } from "react-native-safe-area-context";
+import { View, Text, StyleSheet } from "react-native";
 
-const Goals = () => {
+export default function Index() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>Your Goals</Text>
-
+      <Text style={styles.title}>Welcome to TaskPal 🎯</Text>
+      <Text style={styles.subtitle}>Your Goals</Text>
     </SafeAreaView>
-  )
+  );
 }
-
-export default Goals
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 30,
-    backgroundColor: '#471a1aff'
+    backgroundColor: "#471a1aff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
-    fontSize: 24,
-    textAlign: 'center',
-    marginBottom: 30,
+    fontSize: 28,
+    fontWeight: "bold",
+    color: "#fff",
+    marginBottom: 15,
   },
-})
+  subtitle: {
+    fontSize: 20,
+    color: "#ddd",
+  },
+});
